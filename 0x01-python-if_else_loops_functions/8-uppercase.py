78d148515d1d5951cdce196bb5b3f64dad9bafa8
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-09-21 14:46:47 -0700

    done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= 97 and ord(i):
+        if ord(i) >= 97 and ord(i) <= 123:
             print("{}".format(i))
         else:
-            print()
+            ...
