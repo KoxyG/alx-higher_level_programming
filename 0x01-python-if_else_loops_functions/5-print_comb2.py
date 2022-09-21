d52fef920902c77bd2f30c16c8874e53643e7bb3
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-09-21 15:54:46 -0700

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,5 @@
-#!#usr#bin#python3
-for num in range(0, 99):
-    print('{:02d}, '.format(num), end='')
+#!/usr/bin/python3
+for nums in range(0, 90):
+    if nums == 01 or nums == 09
+        continue
+    print("{:02d}".format(nums), end=", ")
