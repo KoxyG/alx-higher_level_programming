c2f2b634d3cd775a47a418265105b5e22dcc988a
Progress Ochuko Eyaadah <106968712+KoxyG@users.noreply.github.com>
2022-09-22 19:55:43 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     from add_0 import add
 
-    a = 10
-    b = 5
-    add(a, b)
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
