acacaf66bce0884f00d1759430807fb625b2dc66
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-09-29 15:41:43 -0700

    done

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     sum = 0
-    """Using set data type to authomatically remove all the duplicated values"""
     for element in set(my_list):
         sum += element
-        return sum
+    return sum
