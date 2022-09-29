ee538db06780439dd73a1000155c134276813f54
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-09-29 11:25:51 -0700

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    #creating a function to search before replacing
+    """creating a function to search before replacing."""
     def find_search(element):
         return element if element != search else replace
     return list(map(find_search, my_list))
