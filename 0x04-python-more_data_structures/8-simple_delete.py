d50260efbdc9c4fbfa7cb4dc998a19178fbc055c
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-09-29 18:07:57 -0700

    done

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    for key in list(a_dictionary):
-        if key == 'none':
-            return a_dictionary
-        else:
-            del a_dictionary[key]
+    if key == 'None':
+        return a_dictionary
+    else:
+        del a_dictionary[key]
