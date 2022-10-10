6a91b95e3394c95ff76a52d3a35977180a1ad590
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-10-10 16:58:36 -0700

    done

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,5 +8,4 @@ def safe_print_division(a, b):
         result = None
     finally:
         print("Inside result: {}".format(result))
-        print("{} / {} = {}".format(a, b, result))
     return (result)
