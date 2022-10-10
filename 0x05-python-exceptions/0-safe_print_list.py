d1fa03219d53bd97421af4d03a2c402bcc7b8d60
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-10-10 11:44:31 -0700

    done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     try:
-        x = 0
+        total = 0
         for i in range(x):
-            print(f"{my_list[i]}", end=")
-            x += 1
-            return x
+            print(f"{my_list[i]}", end="")
+            total += 1
     except IndexError:
-        break
-    print()
-    return x
+        print("")
+        return total
