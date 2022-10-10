a65353af26781b9f3642fb62a457ab582df46fff
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-10-10 16:44:20 -0700

    done

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,9 +5,9 @@ def safe_print_division(a, b):
     try:
         result = a / b
         return result
-    except ZeroDivisionError:
+    except (TypeError, ZeroDivisionError):
         return "None"
     finally:
-        print("Inside result: ", result)
+        print("Inside result: ".format(result))
         print("{} / {} = {}".format(a, b, result))
     return (result)
