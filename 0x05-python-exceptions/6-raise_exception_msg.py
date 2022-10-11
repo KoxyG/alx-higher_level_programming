ee42a91cad3bc71eefdee3cc373ce97ad97efcd5
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-10-10 17:43:20 -0700

    done

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    try:
-        raise NameError("Hi, its Koxy :)")
-    except NameError:
-        print("There was an exception")
-        raise
+    raise NameError("Hi, its Koxy :)
