b08c66d4c604a11f7d6ae46aa9a778ca6986f541
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-10-10 17:35:34 -0700

    done

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def raise_exception():
-    try:
-        raise _exception()
-    except TypeError:
-        print("Exception raised")
-        raise
+    """Raise type error exception"""
+
+    raise TypeError
