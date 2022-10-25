24420f496af863e7143532e66ddc2d9479fb2299
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-10-25 10:33:19 -0700

    done

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a module that inherits the list from a class
+"""
+
+
+class MyList(list):
+    pass
+
+"""A class that inherits the list"""
+    def print_sorted(self):
+        """prints a sorted list""
+        print(sorted(self))
