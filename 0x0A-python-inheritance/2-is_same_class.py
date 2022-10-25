f80b4d3bceb88115a728365347d9613fb8510a63
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-10-25 15:47:37 -0700

    done

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""checks if objects is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true if object is an instance of thr class, otherwise return false
+    """
+    if isinstance(obj, class):
+        return True
+    else:
+        return False
