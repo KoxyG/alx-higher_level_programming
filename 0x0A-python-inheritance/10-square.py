1c94cb7a820ca1a38748de86a4faa0384322d073
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-10-26 05:30:55 -0700

    done

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""inherits from BaseGeometry"""
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """a square class that inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize a new square
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
