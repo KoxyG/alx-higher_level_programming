a6cd59df1f23d625d6f9dbf1713f9435a9ea6612
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-10-27 15:40:02 -0700

    done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """This module defines a text file-reading function"""
+
+
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
