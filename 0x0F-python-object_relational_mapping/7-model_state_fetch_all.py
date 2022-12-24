5605cb20a669fa78ada6c02228f82abfb5018cf7
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-12-24 13:02:23 -0800

    done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 """
-This is a script that lists all State objects from the database hbtn_0e_6_usa
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
 """
 
-import sys
-from model_state import Base, State
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ = "__main__":
-    db_url = "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
 
     engine = create_engine(db_url)
-    session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
 
     session = Session()
 
