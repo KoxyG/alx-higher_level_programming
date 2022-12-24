29dfd8130c69ba481cdeaf868b18364d32872709
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-12-24 07:12:39 -0800

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-""" a script that lists all states from the database hbtn_0e_0_usa """
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
 import MySQLdb
-import sys
+from sys import argv
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     """
     Access to the database and get the states
     from the database.
     """
-    db_connect = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-         db=argv[3], port=3306)
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+
     db_cursor = db_connect.cursor()
+
     db_cursor.execute("SELECT * FROM states")
+
     rows_selected = db_cursor.fetchall()
+
     for row in rows_selected:
         print(row)
-    cur.close()
-    db.close()
