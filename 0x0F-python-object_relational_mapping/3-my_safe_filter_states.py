cc1d54fcb703d85d91fd8ffbe414929e14d1bb7e
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2022-12-24 09:59:56 -0800

    done

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
 """
-This is a script that is safe frim MySQL injection that takes in arguments and displays all values
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
 """
 
-import MySQLdb
-import sys
-
+import MySQLdb as db
+from sys import argv
 
 if __name__ == "__main__":
-    db_connect = db.connect(host="localhost", port=3306, user=argv[1],
-                            passwd=argv[2], db=argv[3])
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
     db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM states WHERE  name LIKE \
-                      BINARY % (name)s ORDER BY states.id ASC", {'name': argv[4]})
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
 
     rows_selected = db_cursor.fetchall()
 
