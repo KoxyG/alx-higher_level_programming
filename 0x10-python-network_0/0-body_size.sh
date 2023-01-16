785978c19ef52600be6b6a692379ca110f3452bf
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-01-16 12:23:35 -0800

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This is a script that takes in and send a request to that URL to display the size of the body response
 
-curl -s "$1" | wc -c
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
