0c3f72ab4d2b0725189627d84ff4815c838fba7f
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-01-18 07:19:16 -0800

    done

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""This is a script that takes in a URL,
+- sends a request to the URL and displays the body of the response
+- manage urllib.error.HTTPError
+- exceptions and print: Error code
+- followed by the HTTP status code
+"""
+
+import sys
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as req:
+            print(req.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
