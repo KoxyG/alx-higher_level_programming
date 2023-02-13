a6429e3e5e631ffafc74430eef5c8e9a3bba2ea5
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-02-12 18:35:17 -0800

    done

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
-
-// a script that prints all characters of a Star Wars movie
-// The first argument is the Movie ID - example: 3 = “Return of the Jedi”
-// Display one character name by line in the same order of the 
-// list “characters” in the /films/ response
-// You must use the Star wars API
-// You must use the module request
+//  a script that prints all characters of a Star Wars movie
+//  The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+//  Display one character name by line in the same order of the
+//  list “characters” in the /films/ response
+//  You must use the Star wars API
+//  You must use the module request
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/`;
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
 let characters = [];
 
 request(url, (error, response, body) => {
